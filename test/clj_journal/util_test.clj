aca6b05b0c8eaf4b8a26717e6658cea484d34023
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2019-05-14 12:39:28 +0200

    Add tests for weird characters in field names

--- test/clj_journal/util_test.clj
+++ test/clj_journal/util_test.clj
@@ -4,14 +4,19 @@
 
 (deftest utility
   (is (= (args->journal-fields "some" 12 "value" "message")
-        ["SOME=12" "VALUE=message"])
-    "Should return a vector of strings")
+         ["SOME=12" "VALUE=message"])
+      "Should return a vector of strings")
 
   (is (thrown? AssertionError (args->journal-fields "some" 12 "value"))
-    "Should throw an AssertionError when passed an uneven number of arguments")
+      "Should throw an AssertionError when passed an uneven number of arguments")
 
   (is (= (level->syslog :err) 3) "`err` is syslog level 3")
   (is (= (level->syslog 3) 3) "Integer arguments are returned as-is")
   (is (thrown? java.lang.IllegalArgumentException
-        (level->syslog :notanerrorlevel))
-    "Should thrown an exception on invalid log level"))
+               (level->syslog :notanerrorlevel))
+      "Should thrown an exception on invalid log level"))
+
+(deftest names
+  (is (= (to-field-name :active-check) "ACTIVE_CHECK"))
+  (is (= (to-field-name :active/check) "ACTIVE_CHECK"))
+  (is (= (to-field-name :active-_/_-%check) "ACTIVE______CHECK")))
