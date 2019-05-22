1098dc82e9c72157c95968a8e848ebd8722a7083
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2019-05-22 10:24:21 +0200

    Add tests for `%n` in writable segment error

--- test/clj_journal/log_test.clj
+++ test/clj_journal/log_test.clj
@@ -6,13 +6,15 @@
   (testing "print"
     (is (nil? (jprint :err "test! %s" "a string!")))
     (is (nil? (jprint :err "testing more: %x" 3735928559)))
+    (is (nil? (jprint :err "testing more: %n" 3735928559)))
 
     (is (nil?
-          (binding [clj-journal.util/level->syslog (fn [_] 1)]
-            (jprint :debug "hello there"))))
+         (binding [clj-journal.util/level->syslog (fn [_] 1)]
+           (jprint :debug "hello there"))))
 
     (is (thrown? IllegalArgumentException (jprint :not-a-log-level "booo"))))
 
   (testing "send"
     (is (nil? (jsend :err "test!")))
+    ;; (is (nil? (jsend :err "test! %n")))
     (is (thrown? IllegalArgumentException (jsend :not-a-log-level "booo")))))
